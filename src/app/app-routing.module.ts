cf780f1856eca94b9db6386042f1fd10fb36e7e8
Liang Chen <chenliang0571@gmail.com>
2019-10-30 11:26:17 +0000

    add components and css demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CssDemoComponent } from './css-demo/css-demo.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'css', component: CssDemoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocationBarComponent } from './location-bar/location-bar.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
+import { CategoryComponent } from './category/category.component';
+import { PromotionComponent } from './promotion/promotion.component';
+import { CssDemoComponent } from './css-demo/css-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationBarComponent,
+    SearchBoxComponent,
+    CategoryComponent,
+    PromotionComponent,
+    CssDemoComponent
+  ],
+  exports: [
+    MatInputModule,
+    MatIconModule
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]

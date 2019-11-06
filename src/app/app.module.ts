2b5421662570e433c2c258c3095239ab3f1f82a4
Liang Chen <chenliang0571@gmail.com>
2019-11-06 13:11:54 +0000

    use mat-grid-tile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +14,13 @@ import { SearchBoxComponent } from './search-box/search-box.component';
 import { CategoryComponent } from './category/category.component';
 import { PromotionComponent } from './promotion/promotion.component';
 import { CssDemoComponent } from './css-demo/css-demo.component';
+
+const matModules = [
+  MatInputModule,
+  MatIconModule,
+  MatButtonModule,
+  MatGridListModule
+];
 
 @NgModule({
   declarations: [
@@ -22,15 +32,13 @@ import { CssDemoComponent } from './css-demo/css-demo.component';
     CssDemoComponent
   ],
   exports: [
-    MatInputModule,
-    MatIconModule
+    matModules
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatIconModule
+    matModules
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,13 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tile {
+  text: string;
+  image: string;
+  url: string;
+}
+
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent implements OnInit {
 
-  constructor() { }
+export class CategoryComponent implements OnInit {
+  defaultTile = ' mat-grid-tile';
+  categories: Tile[] = [
+    {text: 'food', image: 'food-image', url: ''},
+    {text: 'drink', image: 'drink-image', url: ''},
+    {text: 'fruit', image: 'fruit-image', url: ''},
+    {text: 'food', image: 'food-image', url: ''},
+    {text: 'drink', image: 'drink-image', url: ''},
+    {text: 'fruit', image: 'fruit-image', url: ''},
+    {text: 'food', image: 'food-image', url: ''},
+    {text: 'drink', image: 'drink-image', url: ''},
+    {text: 'fruit', image: 'fruit-image', url: ''},
+    {text: 'food', image: 'food-image', url: ''},
+    {text: 'drink', image: 'drink-image', url: ''},
+    {text: 'fruit', image: 'fruit-image', url: ''},
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
